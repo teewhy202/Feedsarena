51f24ec707c7c4e7e74bf11e6cd6896e641fcab6
teewhy202 <136259108+teewhy202@users.noreply.github.com>
2023-06-24 20:08:32 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://www.feedsarena.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
