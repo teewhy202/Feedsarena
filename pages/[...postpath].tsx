d4a5d58922c98bb6c76bfd06f9f7f8c885328e9d
teewhy202 <136259108+teewhy202@users.noreply.github.com>
2023-10-31 01:17:22 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://www.feedsarena.com/graphql"
+	const endpoint = "https://www.schoolgist.live/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
